d33b5b14c4c2f78b2536359d472bef2a70557088
juatriesp <juantrigueroespejo@gmail.com>
2025-01-20 17:54:04 +0100

    Creación componente footer e implementación Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ComponenteJuanComponent } from './componente-juan/componente-juan.component';
 import { ComponenteRaulComponent } from './componente-raul/componente-raul.component';
 import { ComponenteAntonioComponent } from './componente-antonio/componente-antonio.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponenteJuanComponent,
     ComponenteRaulComponent,
-    ComponenteAntonioComponent
+    ComponenteAntonioComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
