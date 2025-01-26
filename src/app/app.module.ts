5c2781cc1ea6f1bd565e689cbb121e7b5dda9fdf
juatriesp <juantrigueroespejo@gmail.com>
2025-01-26 19:05:43 +0100

    componente footer con fallos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ComponenteJuanComponent } from './componente-juan/componente-juan.component';
 import { ComponenteRaulComponent } from './componente-raul/componente-raul.component';
 import { ComponenteAntonioComponent } from './componente-antonio/componente-antonio.component';
+import { FooterComponenteComponent } from './footer-componente/footer-componente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponenteJuanComponent,
     ComponenteRaulComponent,
-    ComponenteAntonioComponent
+    ComponenteAntonioComponent,
+    FooterComponenteComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/footer-componente/footer-componente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-componente',
+  standalone: false,
+  
+  templateUrl: './footer-componente.component.html',
+  styleUrl: './footer-componente.component.css'
+})
+export class FooterComponenteComponent {
+
+}
