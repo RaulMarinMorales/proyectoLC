2c2309f87b13ffea4d8339f594f5265b8e2fa8ff
Valverde-Antonio <antoniovalverde6114@gmail.com>
2025-02-03 11:52:27 +0100

    Subiendo cambios a develop-antonio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { ComponenteJuanComponent } from './componente-juan/componente-juan.component';
 import { ComponenteRaulComponent } from './componente-raul/componente-raul.component';
 import { ComponenteAntonioComponent } from './componente-antonio/componente-antonio.component';
+import { HeaderComponent } from './header/header.component';
+import { FormularioNatacionComponent } from './formulario-natacion/formulario-natacion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponenteJuanComponent,
     ComponenteRaulComponent,
-    ComponenteAntonioComponent
+    ComponenteAntonioComponent,
+    HeaderComponent,
+    FormularioNatacionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/formulario-natacion/formulario-natacion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-natacion',
+  standalone: false,
+  
+  templateUrl: './formulario-natacion.component.html',
+  styleUrl: './formulario-natacion.component.css'
+})
+export class FormularioNatacionComponent {
+
+}
