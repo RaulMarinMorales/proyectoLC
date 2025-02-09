27bbdaf1e5f1979210ccdb24d42ce331722fe9de
Valverde-Antonio <antoniovalverde6114@gmail.com>
2025-02-09 21:00:43 +0100

    Formulario Natacion y Eventos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // 📌 IMPORTA ESTO
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +21,8 @@ import { FormularioNatacionComponent } from './formulario-natacion/formulario-na
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
