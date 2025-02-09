ab028d135e972d26c3a65350ecd351a6a36bd579
RaulMarinMorales <raulmarinmorales99@gmail.com>
2025-02-09 20:29:11 +0100

    Subida de varias directivas creadas, cuerpo de la web, formulario de inscripcion a un torneo de futbol y un evento a la hora de registrar un equipo de futbol

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//la siguiente linea es para poder usar ngmodel en el formulario raul
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponenteJuanComponent } from './componente-juan/componente-juan.component';
-import { ComponenteRaulComponent } from './componente-raul/componente-raul.component';
 import { ComponenteAntonioComponent } from './componente-antonio/componente-antonio.component';
+import { CuerpoComponent } from './cuerpo/cuerpo.component';
+import { FormularioRaulComponent } from './formulario-raul/formulario-raul.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponenteJuanComponent,
-    ComponenteRaulComponent,
-    ComponenteAntonioComponent
+    ComponenteAntonioComponent,
+    CuerpoComponent,
+    FormularioRaulComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
